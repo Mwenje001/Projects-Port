1a8346c9b016b7a6bd47c214f6c2642e3ae92d4b
Mwenje01 <mwenje72@gail.com>
2023-05-19 21:46:54 +0300

    More Comits

---
+++ Javascript/Basics/app.js
@@ -0,0 +1,6 @@
+var life = 100;
+
+var names = 'Dev Ed';
+
+
+console.log('Hey', names, 'You are', life - 30, 'years old');
---
+++ Javascript/Slot-Machine/loops.js
@@ -0,0 +1,15 @@
+let loading = 99;
+
+while(loading < 100){
+    console.log('Website is Still Loading!');
+    loading++;
+}
+
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+for(names of numbers){
+    console.log(names);
+    if(names === '5'){
+        
+    }
+}
